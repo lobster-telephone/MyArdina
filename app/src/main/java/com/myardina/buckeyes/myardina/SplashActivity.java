952a57b6984c0925039f1c7e04f50e442e1929b3
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2016-10-16 10:12:50 -0400

    Added splash activity

---
+++ app/src/main/java/com/myardina/buckeyes/myardina/SplashActivity.java
@@ -0,0 +1,48 @@
+package com.myardina.buckeyes.myardina;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Activity for the splash screen
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Remove title bar
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.setContentView(R.layout.activity_splash);
+
+        //thread created to put the activity to sleep for 3 seconds
+        Thread splashTimer = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        splashTimer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
