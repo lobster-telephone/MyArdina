906fc0eaafe7a5b7dfa94f326bf132a09a887597
sachinda <sachinda.yl@gmail.com>
2016-10-18 18:42:36 -0400

    Doctor Activity created, Spinner for availability added

--- app/src/main/java/com/myardina/buckeyes/myardina/DoctorActivity.java
+++ app/src/main/java/com/myardina/buckeyes/myardina/DoctorActivity.java
@@ -2,12 +2,39 @@ package com.myardina.buckeyes.myardina;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class DoctorActivity extends AppCompatActivity {
+public class DoctorActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor);
+
+
+        Spinner doctor_availability_spinner = (Spinner) findViewById(R.id.spinner_doctor_availability);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.doctor_availability_spinner_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        doctor_availability_spinner.setAdapter(adapter);
+        doctor_availability_spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        //if yes is selected doctor is available
+
+        //else not
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
